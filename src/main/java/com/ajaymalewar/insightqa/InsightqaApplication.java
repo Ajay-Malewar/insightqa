7c78435a31efae89a3a9c1d3d5627405cb0ac475
ajaynm45 <ajay.nm45@gmail.com>
2026-08-13 10:41:42 +0530

    Migrate from SimpleVectorStore to PGVector for persistent storage

--- src/main/java/com/ajaymalewar/insightqa/InsightqaApplication.java
+++ src/main/java/com/ajaymalewar/insightqa/InsightqaApplication.java
@@ -1,9 +1,10 @@
 package com.ajaymalewar.insightqa;
 
+import org.springframework.ai.vectorstore.pgvector.autoconfigure.PgVectorStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { PgVectorStoreAutoConfiguration.class })
 public class InsightqaApplication {
 
 	public static void main(String[] args) {
